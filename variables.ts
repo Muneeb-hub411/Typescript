7627f61da8d0029951b049b13aa1603a56beed45
Muneeb-hub411 <muneebsaleem402@gmail.com>
2024-10-07 23:21:28 +0500

    learn about any

--- variables.ts
+++ variables.ts
@@ -5,6 +5,17 @@ let age =10
 
 //typescript will handle these things like make sure that a data should be of the same type as of the variable in which we are storing them
 
+let users
+
+function getuser(){
+    return "Muneeb"
+}
+
+users=getuser
+
+// any when you want to stop typescript from typechecking we use any which is not a good practice
+
+
 
 
 export {}
